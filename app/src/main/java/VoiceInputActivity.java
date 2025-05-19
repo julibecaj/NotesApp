b1aee16c8797ad624c768ff115e7785dc477a980
julibecaj <julianbecaj@unyt.edu.al>
2025-05-19 20:01:37 +0200

    notes app beginning

--- app/src/main/java/VoiceInputActivity.java
+++
@@ -1,5 +0,0 @@
-import androidx.appcompat.app.AppCompatActivity;
-
-public class VoiceInputActivity extends AppCompatActivity {
-
-}
--- app/src/main/java/com/example/notesapp/MainActivity.java
+++ app/src/main/java/com/example/notesapp/MainActivity.java
@@ -1,20 +1,41 @@
 package com.example.notesapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        btn1 = findViewById(R.id.micButton);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new VoiceInputActivity());
+            }
+        });
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -22,23 +43,15 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    //onCreate(Bundle savedInstanceState)—static initialization
-
-    //onStart()—when Activity (screen) is becoming visible
-
-    //onRestart()—called if Activity was stopped (calls onStart())
-
-    //onResume()—start to interact with user
-
-    //onPause()—about to resume PREVIOUS Activity
-
-    //onStop()—no longer visible, but still exists and all state info preserved
-
-    //onDestroy()—final call before Android system destroys Activity
 
 
+    public void replaceFragment(Fragment fragment){
 
+    FragmentManager fragmentManager = getSupportFragmentManager();
+    FragmentTransaction transaction = fragmentManager.beginTransaction();
+    transaction.replace(R.id.frameLayout,fragment);
+    transaction.addToBackStack(null);
+    transaction.commit();
 
-
-
+    }
 }
